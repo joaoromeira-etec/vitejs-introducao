7e55a39e618bf508950edb8239af8abb723ae888
joaoromeira-etec <joaopedrodiasromeira456@gmail.com>
2026-02-27 22:46:27 -0300

    introdução ao vite.js, aula do dia 27 de janeiro

---
+++ src/pages/sobre/index.jsx
@@ -0,0 +1,9 @@
+export default function Sobre () {
+    return(
+        <>
+            <h1>Sobre</h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut maxime rem quae quasi harum similique alias nostrum excepturi culpa quas impedit, unde deserunt mollitia. Consequatur eum reiciendis perferendis numquam voluptates!</p>
+        </>
+    )
+}

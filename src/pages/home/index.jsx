5d9e6554cea28000c24cae04a39dd11ef3567c3b
joaoromeira-etec <joaopedrodiasromeira456@gmail.com>
2026-03-02 22:45:58 -0300

    mudanças na estilização do site por CSS, dia 2 de março.

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,15 @@
+function Home() {
+
+  return (
+    <>
+      <h1>Aula Introdução Vite JS</h1>
+      <h2>Preparo do projeto</h2>
+      <h3>Estilização</h3>
+      <p>Entendendo uso de css dentro do projeto.</p>
+      <label>Exemplo Label</label>
+      <small>Exemplo Small</small>
+    </>
+  )
+}
+
+export default Home

0b712e97ff0c4545a84474eab97e8b80448dcd34
joaoromeira-etec <joaopedrodiasromeira456@gmail.com>
2026-03-09 22:46:32 -0300

    commit mudanças vitejs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Routes, Route} from 'react-router-dom';
 
 import Home from './pages/home';
-import Sobre from './pages/sobre'
+import Exemplo01 from './pages/exemplos/ex-01';
+import Exemplo02 from './pages/exemplos/ex-02';
 
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
     
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/sobre' element={<Sobre/>}/>
+        <Route path='/exemplo/1' element={<Exemplo01/>}/>
+        <Route path='/exemplo/2' element={<Exemplo02/>}/>
       </Routes>
     
   )
---
+++ src/pages/exemplos/ex-01/index.jsx
@@ -0,0 +1,9 @@
+export default function Exemplo01 () {
+    return(
+        <>
+            <h1>Exemplo 1</h1>
+
+            <p>Componente mínimo</p>
+        </>
+    )
+}
